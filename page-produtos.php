0ee263b2a96a5f69add8dfc6784bd12c18768d51
Vinícius Hoyer <vhoyer@live.com>
2017-11-02 00:00:53 -0200

    Changed products to container
    Signed-off-by: Vinícius Hoyer <vhoyer@live.com>

--- page-produtos.php
+++ page-produtos.php
@@ -17,7 +17,7 @@ $query = new WP_Query( [
 	<?php dynamic_heading(get_the_title(), [
 	"class"=> "heading",
 	]); ?>
-	<div class="center-text-within container-fluid">
+	<div class="center-text-within container">
 		<div class='row normalize-anchor'>
 			<?php while ($query->have_posts()) : $query->the_post(); ?>
 			<a href="<?php echo esc_url( get_post_custom_values("product_url", get_the_ID())[0] ) ?>">
